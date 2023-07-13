10a92d68a01a1ed2d44313fe26aef5f5b8fda106
Moyosoore Ayo-Idowu <moyosooreai@gmail.com>
2023-07-13 13:44:41 +0100

    Navbar

--- src/components/navbar/Navigation.js
+++ src/components/navbar/Navigation.js
@@ -1,16 +1,22 @@
+import './Navigation.css';
+
 import Navbar from 'react-bootstrap/Navbar';
 import NavbarBrand from 'react-bootstrap/esm/NavbarBrand';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
 
+import github from '../../assets/images/github.ico';
+//  '../../assets/images/github.ico';
 const Navigation = () => {
   return (
     // <Navbar expand='lg' className='bg-body-tertiary'></Navbar>
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" bg="dark" data-bs-theme="dark">
       <Container>
         <NavbarBrand href="/">Text-Analyzer</NavbarBrand>
-        <Nav>
-          <Nav.Link href="#">Github</Nav.Link>
+        <Nav className="icon-bar">
+          <Nav.Link href="https://github.com/Moyosooreoluwa" target="__blank">
+            <img src={github} className="icon" alt="" />
+          </Nav.Link>
         </Nav>
       </Container>
     </Navbar>
